e19f381cfc8350c9c68b234f22cacd9d536cb05c
izumi-rui23 <r-izumi@e3sys.co.jp>
2026-03-13 14:44:54 +0900

    fix:コンマの位置と、無駄な半角スペース削除の修正

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -32,9 +32,9 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm front-medium md:flex-none md:justify-start md:p-2 md:px-3'
-             , {
-                'bg-sky-100 ': pathname === link.href,
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm front-medium md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100': pathname === link.href,
               }
             )}
           >
